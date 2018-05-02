04c80a501ff304a503ab86b296812d7dc8687190
DSkave <xecs66bullets@gmail.com>
2018-05-02 11:04:11 +0900

    WinNoteから　Gitの勉強１

--- KTest/settings.py
+++ KTest/settings.py
@@ -78,6 +78,19 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME':'KSystem',
+        #'USER':'root',
+        #'PASSWORD':'xecs_blades',
+        #'HOST':'192.168.0.7',
+        #'PORT':'8889',
+        #'OPTIONS':
+        #{
+        #    'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        #},
+        #'TEST': {
+        #    'NAME': 'test_sample'
+        #}
     }
 }
 
--- manage.py
+++ manage.py
@@ -2,6 +2,9 @@
 import os
 import sys
 
+#import pymysql
+
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "KTest.settings")
     try:
