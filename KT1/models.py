49eb7c77f0c71e9e9967aaee3a6a78a884c9fc8a
DSkave <xecs66bullets@gmail.com>
2018-05-21 14:39:33 +0900

    モデル修正　Integerfield のvalidator削除

--- KT1/models.py
+++ KT1/models.py
@@ -4,8 +4,8 @@ from django.utils import timezone
 class OddsList(models.Model):
    event_date = models.DateTimeField            #日付
    place      = models.CharField(max_length=30) #競馬場名
-   race_no    = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(12)]) #レース番号
-   horse_no   = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(18)])#馬番
+   race_no    = models.IntegerField() #レース番号
+   horse_no   = models.IntegerField() #馬番
    win_odds   = models.DecimalField(max_digits=4,decimal_places=1) #単勝オッズ
    show_odds1 = models.DecimalField(max_digits=4,decimal_places=1) #複勝オッズ
    show_odds2 = models.DecimalField(max_digits=4,decimal_places=1) #複勝オッズ２
